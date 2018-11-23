ddbbf0df9a957a745f88078a1fb3abc03bd950c3
root <emploi.neveu@gmail.com>
2018-11-23 10:32:03 +0100

    exo4

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $months = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre' );
+    $months[7] = 'août';
+    echo $months[7];
+    ?>
+  </body>
+</html>
